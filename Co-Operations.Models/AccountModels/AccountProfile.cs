6feb88a82b8e1fcb780358727c7699611237384b
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2020-10-07 16:07:53 -0400

    Started building profile page

---
+++ Co-Operations.Models/AccountModels/AccountProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Operations.Models.AccountModels
+{
+    public class AccountProfile
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Funds Earned")]
+        public decimal FundsEarned { get; set; }
+
+        [Display(Name = "Funds Payed Out")]
+        public decimal FundsPayedOut { get; set; }
+
+        [Display(Name = "Funds Owed")]
+        public decimal FundsOwed { get; set; }
+
+        
+    }
+}
